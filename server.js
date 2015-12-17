7d072768daf05d55dac6e4ea3795b12f62cfd657
Mayank Gupta <MayankGupta688@Gmail.com>
2015-12-17 15:08:45 +0530

    New Changes

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+// Http is used to create Server to take up the requests
 var http = require('http');
 
+// Express act as a routing Engine 
 var express = require('express');
 
+// Express is initialized to carry ot routing process
 var app = express();
 
 app.get('/', function(req, res) {
   res.send("Hi All");
 })
 
+// Server is created and express is used as a middleware to configure the routes
 var server = http.createServer(app);
 
+// Apllication is made to listen Http request at port 3000
 app.listen(3000);
