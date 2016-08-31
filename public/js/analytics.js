7b0458346137aca7957006f00c3e69c6c5b8e283
Mayank Gupta <MayankGupta688@Gmail.com>
2016-08-31 10:14:22 +0530

    Com

---
+++ public/js/analytics.js
@@ -0,0 +1,11 @@
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-83101907-1', 'auto');
+ga('send', 'pageview');
+ga('send', 'event', { eventCategory: 'Scrolled 250px', eventAction: 'Scroll' });
+
+setTimeout("ga('send', 'event', { eventCategory: '3 seconds on page', eventAction: 'Read' })", 3000);
