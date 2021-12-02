8a6d28e8ebe73fd0604a0e3aebed589b42860f23
SMJen1 <83735884+SMJen1@users.noreply.github.com>
2021-12-01 19:47:19 -0800

    full path to exe

--- execute.bat
+++ execute.bat
@@ -1,4 +1,4 @@
-CALL npm install
-CALL npm install pm2 -g
-CALL npm stop all
-CALL npm start
+CALL "C:\Program Files\nodejs\node.exe"    install
+CALL "C:\Program Files\nodejs\node.exe" install pm2 -g
+CALL "C:\Program Files\nodejs\node.exe"  stop all
+CALL "C:\Program Files\nodejs\node.exe" start
